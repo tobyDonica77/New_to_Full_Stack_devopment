c5eadfb2947692bcd3d66dca84de6c999b085545
tobyDonica <tdonica@ICLOUD.COM>
2019-08-14 14:37:22 -0500

    Create arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,11 @@
+var amazingCars= ["BMW","Lexus","Ford","Mercedes"];
+      
+    
+      amazingCars.pop();
+      console.log(amazingCars);
+
+      amazingCars.sort();
+      console.log(amazingCars);
+
+      amazingCars.length();
+      console.log(amazingCars)
